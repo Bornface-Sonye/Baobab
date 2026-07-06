aff5a9fff5646c142b574b8396565a7f9a00e042
Bornface-Sonye <bornfacesonye@gmail.com>
2026-07-05 22:44:55 -0700

    THE MODELS ARE SOME HOW UPDATE

--- invest/models.py
+++ invest/models.py
@@ -24,22 +24,14 @@ from .validators import validate_kenyan_phone_number, validate_kenyan_id
 # ===========================================
 
 class Investor(models.Model):
-
-    national_id_no = models.CharField(
-        max_length=8,
-        unique=True,
-        validators=[validate_kenyan_id]
-    )
-
-    phone_number = models.CharField(
-        max_length=13,
-        unique=True,
-        validators=[validate_kenyan_phone_number]
-    )
-
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
+    
+    national_id_no = models.DecimalField(primary_key=True, max_digits=8, decimal_places=0, unique=True, validators=[validate_kenyan_id], help_text="Enter National Identification Number in the format 35033637")
+    email_address = models.EmailField(max_length=200, help_text="Please Enter Investor Email Address")
+    username = models.EmailField(unique=True, max_length=200, help_text="Enter a valid Username")
+    first_name = models.CharField(max_length=150, help_text="Enter Your First Name")
+    last_name = models.CharField(max_length=150, help_text="Enter Your Last Name")
+    phone_number = models.CharField(max_length=15, validators=[validate_kenyan_phone_number], help_text="Enter phone number in the format 0798073204 or +254798073404")
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.national_id_no
@@ -51,75 +43,27 @@ class Investor(models.Model):
 
 class Wallet(models.Model):
 
-    investor=models.ForeignKey(
-        Investor,
-        on_delete=models.CASCADE
-    )
-
-    available_balance = models.DecimalField(
-        max_digits=15,
-        decimal_places=2,
-        default=0
-    )
-
-    locked_balance = models.DecimalField(
-        max_digits=15,
-        decimal_places=2,
-        default=0
-    )
-
-    borrowed_balance = models.DecimalField(
-        max_digits=15,
-        decimal_places=2,
-        default=0
-    )
-
-    collateral_balance = models.DecimalField(
-        max_digits=15,
-        decimal_places=2,
-        default=0
-    )
+    investor=models.ForeignKey(Investor, on_delete=models.CASCADE, help_text="Please Enter Investor Email Address")
+    available_balance = models.DecimalField(max_digits=15, decimal_places=2, default=0, help_text="Please Enter Available Wallet Balance")
+    locked_balance = models.DecimalField(max_digits=15, decimal_places=2, default=0, help_text="Please Enter Locked Balance")
+    borrowed_balance = models.DecimalField(max_digits=15, decimal_places=2, default=0, help_text="Please Enter Borrowed Balance")
+    collateral_balance = models.DecimalField(max_digits=15, decimal_places=2, default=0, help_text="Please Enter Collateral Balance")
 
     def __str__(self):
-        return f"{self.investor.username} Wallet"
+        return f"{self.investor.last_name} Wallet"
 
 
-# ===========================================
-# LIQUIDITY POOL
-# ===========================================
+# ===================================================================================================
+# LIQUIDITY POOL: THIS TABLE IS UPDATED EVERY 10 SECONDS, IT UPDATES WITH THE GRAPH ON THE DASHBOARD
+# ===================================================================================================
 
 class LiquidityPool(models.Model):
 
-    total_available=models.DecimalField(
-        max_digits=15,
-        decimal_places=2,
-        default=1000000
-    )
-
-    total_borrowed=models.DecimalField(
-        max_digits=15,
-        decimal_places=2,
-        default=0
-    )
-
-    total_invested=models.DecimalField(
-        max_digits=15,
-        decimal_places=2,
-        default=0
-    )
-
-    total_collateral=models.DecimalField(
-        max_digits=15,
-        decimal_places=2,
-        default=0
-    )
-
-    current_interest_rate=models.DecimalField(
-        max_digits=5,
-        decimal_places=2,
-        default=10
-    )
-
+    total_available=models.DecimalField(max_digits=15, decimal_places=2, default=1000000, help_text="Please Enter Total Available Amount")
+    total_borrowed=models.DecimalField(max_digits=15, decimal_places=2, default=0, help_text="Please Enter Total Amount Borrowed")
+    total_invested=models.DecimalField(max_digits=15, decimal_places=2, default=0, help_text="Please Enter Total Amount Invested")
+    total_collateral=models.DecimalField(max_digits=15, decimal_places=2, default=0, help_text="Please Enter Total Collateral Amount")
+    current_interest_rate=models.DecimalField(max_digits=5, decimal_places=2, default=10, help_text="Please Enter Current Interest Rate")
 
 
 # ===========================================
@@ -134,39 +78,13 @@ class Investment(models.Model):
         ('RELEASED','RELEASED')
     )
 
-    investor=models.ForeignKey(
-        Investor,
-        on_delete=models.CASCADE
-    )
-
-    amount=models.DecimalField(
-        max_digits=15,
-        decimal_places=2
-    )
-
-    interest_rate=models.DecimalField(
-        max_digits=5,
-        decimal_places=2
-    )
-
+    investor=models.ForeignKey(Investor, on_delete=models.CASCADE, help_text="Please Enter Investor Email Address")
+    amount=models.DecimalField(max_digits=15,decimal_places=2, help_text="Please Enter Amount Invested")
+    interest_rate=models.DecimalField(max_digits=5, decimal_places=2, help_text="Please Enter Interest Rate for Investment")
     duration_days=models.IntegerField()
-
-    expected_return=models.DecimalField(
-        max_digits=15,
-        decimal_places=2,
-        default=0
-    )
-
-    status=models.CharField(
-        max_length=20,
-        choices=STATUS,
-        default='ACTIVE'
-    )
-
-    start_date=models.DateTimeField(
-        auto_now_add=True
-    )
-
+    expected_return=models.DecimalField(max_digits=15, decimal_places=2, default=0, help_text="Please Enter Expected Return")
+    status=models.CharField(max_length=20, choices=STATUS, default='ACTIVE', help_text="Please Select Investment Status")
+    start_date=models.DateTimeField(auto_now_add=True)
     end_date=models.DateTimeField()
 
 
@@ -183,103 +101,41 @@ class Loan(models.Model):
         ('LIQUIDATED','LIQUIDATED')
     )
 
-    borrower=models.ForeignKey(
-        Investor,
-        on_delete=models.CASCADE
-    )
-
-    principal=models.DecimalField(
-        max_digits=15,
-        decimal_places=2
-    )
-
-    collateral=models.DecimalField(
-        max_digits=15,
-        decimal_places=2
-    )
-
-    interest_rate=models.DecimalField(
-        max_digits=5,
-        decimal_places=2
-    )
-
-    amount_due=models.DecimalField(
-        max_digits=15,
-        decimal_places=2
-    )
-
+    borrower=models.ForeignKey(Investor, on_delete=models.CASCADE, help_text="Please Select Investor")
+    principal=models.DecimalField(max_digits=15, decimal_places=2, help_text="Please Enter Principal Amount")
+    collateral=models.DecimalField(max_digits=15, decimal_places=2, help_text="Please Enter Collateral Amount")
+    interest_rate=models.DecimalField(max_digits=5, decimal_places=2, help_text="Please Enter Interest Rate")
+    amount_due=models.DecimalField(max_digits=15, decimal_places=2, help_text="Please Enter Amount Due")
     duration_days=models.IntegerField()
-
     due_date=models.DateTimeField()
+    status=models.CharField(max_length=20, choices=STATUS, default='ACTIVE', help_text="Please Enter Loan Status")
 
-    status=models.CharField(
-        max_length=20,
-        choices=STATUS,
-        default='ACTIVE'
-    )
-
-
-
+'''
 # ===========================================
 # LOAN TRACKER
 # ===========================================
 
 class LoanTracker(models.Model):
 
-    loan=models.ForeignKey(
-        Loan,
-        on_delete=models.CASCADE
-    )
-
-    investment=models.ForeignKey(
-        Investment,
-        on_delete=models.CASCADE
-    )
-
-    amount_allocated=models.DecimalField(
-        max_digits=15,
-        decimal_places=2
-    )
-
-    profit_generated=models.DecimalField(
-        max_digits=15,
-        decimal_places=2,
-        default=0
-    )
-
-    remaining_balance=models.DecimalField(
-        max_digits=15,
-        decimal_places=2
-    )
+    loan=models.ForeignKey(Loan, on_delete=models.CASCADE)
+    investment=models.ForeignKey(Investment, on_delete=models.CASCADE)
+    amount_allocated=models.DecimalField(max_digits=15, decimal_places=2)
+    profit_generated=models.DecimalField(max_digits=15, decimal_places=2, default=0)
+    remaining_balance=models.DecimalField(max_digits=15, decimal_places=2)
+'''
 
 
-
-# ===========================================
-# STOCK
-# ===========================================
-
+# =========================
+# STOCK     (Add symbol field) ; bank shares, eg Equity shares, KPLC shares
+# =========================
 class Stock(models.Model):
-
-    symbol=models.CharField(
-        max_length=20,
-        unique=True
-    )
-
-    name=models.CharField(
-        max_length=200
-    )
-
-    current_price=models.DecimalField(
-        max_digits=15,
-        decimal_places=2
-    )
-
-    available_units=models.IntegerField()
+    stock_code = models.CharField(primary_key=True, max_length=20, help_text="Please Enter Stock Code")
+    stock_name = models.CharField(max_length=200, help_text="Please Enter Stock Name")
+    shares = models.IntegerField(help_text="Enter Number of Available Shares")
+    current_price = models.DecimalField(max_digits=12, decimal_places=2, validators=[MinValueValidator(0)], help_text="Enter Price per Share")
 
     def __str__(self):
-
-        return self.symbol
-
+        return self.stock_name
 
 
 # ===========================================
@@ -294,27 +150,11 @@ class PortfolioHolding(models.Model):
         ('BORROWED','BORROWED')
     )
 
-    investor=models.ForeignKey(
-        Investor,
-        on_delete=models.CASCADE
-    )
-
-    stock=models.ForeignKey(
-        Stock,
-        on_delete=models.CASCADE
-    )
-
+    investor=models.ForeignKey(Investor, on_delete=models.CASCADE, help_text="Please Select Investor")
+    stock=models.ForeignKey(Stock, on_delete=models.CASCADE, help_text="Please Select Stock")
     quantity=models.IntegerField()
-
-    average_buy_price=models.DecimalField(
-        max_digits=15,
-        decimal_places=2
-    )
-
-    fund_source=models.CharField(
-        max_length=20,
-        choices=FUND_SOURCE
-    )
+    average_buy_price=models.DecimalField(max_digits=15, decimal_places=2, help_text="Please Enter Average Buy Price")
+    fund_source=models.CharField(max_length=20, choices=FUND_SOURCE, help_text="Please Select Fund Source")
 
 
 # ===========================================
@@ -328,52 +168,24 @@ class Trade(models.Model):
         ('SELL','SELL')
     )
 
-    investor=models.ForeignKey(
-        Investor,
-        on_delete=models.CASCADE
-    )
-
-    stock=models.ForeignKey(
-        Stock,
-        on_delete=models.CASCADE
-    )
-
-    trade_type=models.CharField(
-        max_length=20,
-        choices=TYPES
-    )
-
+    investor=models.ForeignKey(Investor, on_delete=models.CASCADE, help_text="Please Select Investor")
+    stock=models.ForeignKey(Stock, on_delete=models.CASCADE, help_text="Please Select Stock")
+    trade_type=models.CharField(max_length=20, choices=TYPES, help_text="Please Select Trade Type")
     quantity=models.IntegerField()
-
-    price=models.DecimalField(
-        max_digits=15,
-        decimal_places=2
-    )
-
-    created_at=models.DateTimeField(
-        auto_now_add=True
-    )
+    price=models.DecimalField(max_digits=15, decimal_places=2, help_text="Please Enter Trade Price")
+    created_at=models.DateTimeField(auto_now_add=True)
 
 
-# ===========================================
-# INTEREST HISTORY
-# ===========================================
+# =======================================================================================================
+# INTEREST HISTORY: THIS IS UPDATE FROM THE GRAPH, AS GRAPH MOVES YOU RECORD THE INTEREST DEVIATIONS HERE
+# =======================================================================================================
 
 class InterestHistory(models.Model):
 
-    interest_rate=models.DecimalField(
-        max_digits=5,
-        decimal_places=2
-    )
+    interest_rate=models.DecimalField(max_digits=5, decimal_places=2, help_text="Please Enter Interest Rate")
+    liquidity=models.DecimalField(max_digits=15, decimal_places=2, help_text="Please Enter Liquidity")
+    timestamp=models.DateTimeField(auto_now_add=True)
 
-    liquidity=models.DecimalField(
-        max_digits=15,
-        decimal_places=2
-    )
-
-    timestamp=models.DateTimeField(
-        auto_now_add=True
-    )
 
 # =========================
 # USER AUTH (System_User)
